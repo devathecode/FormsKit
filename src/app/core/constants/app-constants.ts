460a5b8f3ca12f1bc430e1a96a95acfb2f24558a
dev <devanshuv1501@gmail.com>
2022-12-29 23:01:03 +0530

    Build v0.0.14

--- src/app/core/constants/app-constants.ts
+++ src/app/core/constants/app-constants.ts
@@ -6,7 +6,7 @@ export class AppConstants {
      * Form Data
      */
     
-    public static readonly passwordPattern = '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\]).{8,32}$';
+    public static readonly passwordPattern = '(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}';
     public static readonly namePattern = '(^([a-zA-Z]+\\s?)*$)';
     public static readonly messagePattern = '(^(?!\\s)[,`~/;\'<>!#$%+=:.?()"*&@\\w\\s{|}-]*$)';
     public static readonly emailPattern = '(?:[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+)*)@(?:(?:[a' +
