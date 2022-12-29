ee66eb53c6fd280bb04296956c685f92463b42da
Tanshay Garg <gargtanshay@gmail.com>
2022-12-29 19:03:57 +0530

    password validation changes

--- src/app/core/constants/app-constants.ts
+++ src/app/core/constants/app-constants.ts
@@ -6,7 +6,7 @@ export class AppConstants {
      * Form Data
      */
     
-    public static readonly passwordPattern = '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\]).{8,32}$';
+    public static readonly passwordPattern = '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}';
     public static readonly namePattern = '(^([a-zA-Z]+\\s?)*$)';
     public static readonly messagePattern = '(^(?!\\s)[,`~/;\'<>!#$%+=:.?()"*&@\\w\\s{|}-]*$)';
     public static readonly emailPattern = '(?:[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+)*)@(?:(?:[a' +
